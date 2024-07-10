28753801c7fc782a2a9ec14073553b493de9bd0d
KentoMatsuzaki <vantanstudent@outlook.jp>
2024-07-10 19:14:53 +0900

    [add] ステートマシンの汎用メソッド
    遷移メソッドと遷移時のイベントを作成

--- Assets/Script/PlayerStateMachine.cs
+++ Assets/Script/PlayerStateMachine.cs
@@ -1,8 +1,7 @@
-using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.Interactions;
 using Unity.TinyCharacterController.Check;
 using Unity.TinyCharacterController.Control;
+using UnityEngine;
+using UnityEngine.InputSystem;
 
 /// <summary>プレイヤーの状態管理</summary>
 public class PlayerStateMachine : MonoBehaviour
@@ -48,60 +47,60 @@ public class PlayerStateMachine : MonoBehaviour
     /// <summary>無操作状態の処理</summary>
     private void HandleIdleState()
     {
-        Debug.Log("現在のステート：Idle");
+        Debug.Log("プレイヤーの状態：Idle");
     }
 
     /// <summary>歩行中の処理</summary>
     private void HandleWalkingState()
     {
-        Debug.Log("現在のステート：Walk");
+        Debug.Log("プレイヤーの状態：Walking");
     }
 
     /// <summary>走行中の処理</summary>
     private void HandleRunningState()
     {
-
+        Debug.Log("プレイヤーの状態：Running");
     }
 
     /// <summary>ジャンプ中の処理</summary>
     private void HandleJumpingState()
     {
-
+        Debug.Log("プレイヤーの状態：Jumping");
     }
 
     /// <summary>攻撃状態の処理</summary>
     private void HandleAttackState()
     {
-
+        Debug.Log("プレイヤーの状態：Attack");
     }
 
     /// <summary>被ダメージ状態の処理</summary>
     private void HandleDamagedState()
     {
-
+        Debug.Log("プレイヤーの状態：Damaged");
     }
 
     /// <summary>死亡状態の処理</summary>
     private void HandleDeadState()
     {
-
+        Debug.Log("プレイヤーの状態：Dead");
     }
 
-    /// <summary>現在の状態を遷移させる</summary>
-    private void TransitionCurrentState(PlayerState newState)
+    /// <summary>プレイヤーの状態を遷移させる</summary>
+    private void TransitionToOtherState(PlayerState otherState)
     {
         // 現在の状態と遷移先の状態が同じ場合、処理を抜ける
-        if (_currentState == newState) return;
+        if (_currentState == otherState) return;
 
-        // 状態の遷移
-        _currentState = newState;
+        // 現在の状態を遷移させる
+        _currentState = otherState;
 
-        // 状態ごとの処理を呼ぶ
-        OnStateChanged();
+        // イベントを呼ぶ
+        OnStateTransition();
     }
 
-    /// <summary>状態の遷移時に呼ばれる</summary>
-    private void OnStateChanged()
+    /// <summary>各状態の固有処理を実行するイベント。状態の遷移時に呼ばれる。</summary>
+    private void OnStateTransition()
     {
         switch(_currentState)
         {
@@ -116,6 +115,22 @@ public class PlayerStateMachine : MonoBehaviour
             case PlayerState.Running:
                 HandleRunningState();
                 break;
+
+            case PlayerState.Jumping:
+                HandleJumpingState();
+                break;
+
+            case PlayerState.Attack:
+                HandleAttackState();
+                break;
+
+            case PlayerState.Damaged:
+                HandleDamagedState();
+                break;
+
+            case PlayerState.Dead:
+                HandleDeadState();
+                break;
         }
     }
 
@@ -123,24 +138,24 @@ public class PlayerStateMachine : MonoBehaviour
     // 移動のコールバックイベント
     //-------------------------------------------------------------------------------
 
-    /// <summary>移動の物理演算を行う</summary>
-    /// <summary>PlayerInputから呼ばれる</summary>
+    /// <summary>移動の制御をするコールバックイベント</summary>
+    /// <summary>PlayerInputコンポーネントから呼ばれる</summary>
     public void OnMove(InputAction.CallbackContext context)
     {
-        // 歩行状態に遷移
-        TransitionCurrentState(PlayerState.Walking);
+        // 歩行状態に遷移する
+        TransitionToOtherState(PlayerState.Walking);
 
         // 入力値が閾値（Press）以上になった場合
         if (context.performed)
         {
-            // 移動方向を計算
+            // 入力値を元に計算した移動方向へと移動させる
             _moveControl.Move(context.ReadValue<Vector2>());
         }
 
         // 入力値が閾値（Release）以下になった場合
-        else if (context.canceled)
+        if (context.canceled)
         { 
-            // 移動方向を無効化
+            // 移動させない
             _moveControl.Move(Vector2.zero);
         }
     }
