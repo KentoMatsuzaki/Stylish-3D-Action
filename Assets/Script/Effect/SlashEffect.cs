7ced6afad7c3eb82a89f176513289dbc48ce75e1
KentoMatsuzaki <vantanstudent@outlook.jp>
2024-08-05 15:15:39 +0900

    [add] 斬撃エフェクト
    斬撃エフェクトの派生クラスを実装

---
+++ Assets/Script/Effect/SlashEffect.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+/// <summary>斬撃エフェクト</summary>
+public class SlashEffect : AttackEffectBase
+{
+    protected override void Start()
+    {
+        // 基底クラスのStartメソッドを呼び出す
+        base.Start();
+    }
+
+    protected override void UniqueAction()
+    {
+        
+    }
+}
