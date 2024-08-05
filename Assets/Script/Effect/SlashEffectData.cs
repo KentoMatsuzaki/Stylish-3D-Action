e3dc8597c867c3dfe3826dea5e15c1a53e0bcdc7
KentoMatsuzaki <vantanstudent@outlook.jp>
2024-08-05 16:57:10 +0900

    [add] 斬撃エフェクト
    斬撃エフェクトのデータを管理するスクリプタブルオブジェクトを実装。

---
+++ Assets/Script/Effect/SlashEffectData.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+/// <summary>斬撃エフェクトのデータをまとめたクラス</summary>
+[CreateAssetMenu(fileName = "SlashEffectData", menuName = "ScriptableObjects/SlashEffectData")]
+public class SlashEffectData : ScriptableObject
+{
+    /// <summary>斬撃の付加効果</summary>
+    public SlashEnchantment _enchantment;
+
+    /// <summary>斬撃エフェクト(右)</summary>
+    public GameObject _rightSlashEffect;
+
+    /// <summary>斬撃エフェクト(左)</summary>
+    public GameObject _leftSlashEffect;
+}
