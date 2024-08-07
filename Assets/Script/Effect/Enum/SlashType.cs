ab853381bbb2e4947a011343ced438e332b9a3f9
KentoMatsuzaki <vantanstudent@outlook.jp>
2024-08-07 19:43:22 +0900

    [update] 攻撃エフェクトの列挙型

---
+++ Assets/Script/Effect/Enum/SlashType.cs
@@ -0,0 +1,12 @@
+/// <summary>aŒ‚‚Ìí—Ş‚ğ•\‚·—ñ‹“Œ^</summary>
+public enum SlashType
+{
+    /// <summary>…•½</summary>
+    Horizontal,
+
+    /// <summary>a‚èã‚°</summary>
+    Lower,
+
+    /// <summary>a‚è‰º‚°</summary>
+    Upper
+}
