a9860022ba0a7cc6276a834c84ed6f402e334986
KentoMatsuzaki <vantanstudent@outlook.jp>
2024-08-20 18:13:37 +0900

    [update] エフェクトの回転
    斬り上げ・斬り下げエフェクトの回転方向を実装

--- Assets/Script/Effect/Derived/AltEffect.cs
+++ Assets/Script/Effect/Derived/AltEffect.cs
@@ -7,20 +7,45 @@ public class AltEffect : AttackEffectBase
     /// <summary>パーティクルを停止させるまでの遅延時間</summary>
     [SerializeField] private float _delayDuration = 0.25f;
 
+    /// <summary>Z軸方向の回転角度(右)</summary>
+    [SerializeField] private float _rotationZAxisRight = 135f;
+
+    /// <summary>Z軸方向の回転角度(左)</summary>
+    [SerializeField] private float _rotationZAxisLeft = 45f;
+
+    /// <summary>斬り上げるエフェクトかどうか</summary>
+    [SerializeField] private bool _isLowerEffect = false;
+
+    /// <summary>右方向のエフェクトかどうか</summary>
+    [SerializeField] private bool _isRightEffect = false;
+
     private ParticleSystem _particle;
-
-    protected override void Start()
-    {
-        // 基底クラスのStartメソッドを呼び出す
-        base.Start();
-
-        // 固有の処理を呼び出す
-        UniqueAction();
-    }
 
     /// <summary>特殊エフェクト固有の処理</summary>
     protected override void UniqueAction()
     {
+        // 右方向・斬り上げ
+        if (_isLowerEffect && _isRightEffect)
+        {
+            transform.rotation *= Quaternion.Euler(0, 0, -_rotationZAxisRight);
+        }
+        // 左方向・斬り上げ
+        else if (_isLowerEffect && !_isRightEffect)
+        {
+            transform.rotation *= Quaternion.Euler(0, 0, -_rotationZAxisLeft);
+        }
+        // 右方向・斬り下げ
+        else if (!_isLowerEffect && _isRightEffect)
+        {
+            transform.rotation *= Quaternion.Euler(0, 0, _rotationZAxisRight);
+        }
+        // 左方向・斬り上げ
+        else
+        {
+            transform.rotation *= Quaternion.Euler(0, 0, _rotationZAxisLeft);
+        }
+
+        // パーティクルの再生を停止させる
         _particle = GetComponent<ParticleSystem>();
         StartCoroutine(StopParticleWithDelay());
     }
