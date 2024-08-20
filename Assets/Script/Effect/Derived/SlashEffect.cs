640474ce6370ff8f2b5ed02302cced67c540cb52
KentoMatsuzaki <vantanstudent@outlook.jp>
2024-08-20 16:49:51 +0900

    [add] 斬撃エフェクト
    斬撃エフェクトの固有処理でz軸の回転処理を実装

--- Assets/Script/Effect/Derived/SlashEffect.cs
+++ Assets/Script/Effect/Derived/SlashEffect.cs
@@ -3,15 +3,21 @@ using UnityEngine;
 /// <summary>斬撃エフェクト</summary>
 public class SlashEffect : AttackEffectBase
 {
+    [SerializeField] bool _isRightEffect = true;
     protected override void Start()
     {
         // 基底クラスのStartメソッドを呼び出す
         base.Start();
+
+        UniqueAction();
     }
 
     /// <summary>斬撃エフェクト固有の処理</summary>
     protected override void UniqueAction()
     {
-        
+        if (_isRightEffect)
+        {
+            transform.rotation *= Quaternion.Euler(0, 0, 180f);
+        }
     }
 }
