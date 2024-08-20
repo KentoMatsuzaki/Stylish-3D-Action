12100ad1dcdc59735d874aa5263aed500c6723d8
KentoMatsuzaki <vantanstudent@outlook.jp>
2024-08-20 20:13:15 +0900

    [update] 特殊エフェクト
    特殊エフェクトの回転処理を最終調整

--- Assets/Script/Effect/Derived/AltEffect.cs
+++ Assets/Script/Effect/Derived/AltEffect.cs
@@ -24,6 +24,20 @@ public class AltEffect : AttackEffectBase
     /// <summary>特殊エフェクト固有の処理</summary>
     protected override void UniqueAction()
     {
+        // エフェクトを回転させる
+        RotateZAxis();
+
+        // パーティクルの再生を停止させる
+        _particle = GetComponent<ParticleSystem>();
+        StartCoroutine(StopParticleWithDelay());
+
+        // プレハブの回転を終えてから移動させるために、このタイミングで攻撃クラスをアクティブにする
+        GetComponent<AltEffectAttacker>().enabled = true;
+    }
+
+    /// <summary>エフェクトのプレハブをZ軸方向に回転させる</summary>
+    private void RotateZAxis()
+    {
         // 右方向・斬り上げ
         if (_isLowerEffect && _isRightEffect)
         {
@@ -40,14 +54,10 @@ public class AltEffect : AttackEffectBase
             transform.rotation *= Quaternion.Euler(0, 0, _rotationZAxisRight);
         }
         // 左方向・斬り上げ
-        else
+        else if (!_isLowerEffect && !_isRightEffect)
         {
             transform.rotation *= Quaternion.Euler(0, 0, _rotationZAxisLeft);
         }
-
-        // パーティクルの再生を停止させる
-        _particle = GetComponent<ParticleSystem>();
-        StartCoroutine(StopParticleWithDelay());
     }
 
     /// <summary>一定時間待機した後に、パーティクルの再生を停止させる</summary>
