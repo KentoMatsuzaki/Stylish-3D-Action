811ff4f9972d2ad04de76430903495768f2e1ce5
KentoMatsuzaki <vantanstudent@outlook.jp>
2024-08-21 16:42:30 +0900

    [add] ChaseSettings
    ロボットの追跡アクションに関する設定項目をまとめたスクリプタブルオブジェクトのクラスを実装。

---
+++ Assets/Script/Enemy/Scriptable/ChaseSettings.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+/// <summary>ロボットの追跡アクションに関する設定項目</summary>
+
+[CreateAssetMenu(fileName = "ChaseSettings", menuName = "ScriptableObjects/ChaseSettings")]
+
+public class ChaseSettings : ScriptableObject
+{
+    /// <summary>追跡する際の移動速度</summary>
+    [Header("追跡時の移動速度")] public float _chaseSpeed = 1f;
+
+    /// <summary>追跡する際の回転補完のスピード係数</summary>
+    [Header("追跡時の回転補完係数")] public float _chaseRotationDuration = 3f;
+
+    /// <summary>追跡対象を感知する距離</summary>
+    [Header("追跡対象を感知する距離")] public float _playerDetectionRange = 5f;
+
+    /// <summary>追跡対象に到達したかを判定する閾値</summary>
+    [Header("追跡対象への到達閾値")] public float _chaseArrivalThreshold = 2.5f;
+}
