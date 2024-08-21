daf6ce1e5367a0920ee3a9012b356f5c5bc61e6e
KentoMatsuzaki <vantanstudent@outlook.jp>
2024-08-21 16:43:04 +0900

    [add] PatrolSettings
    ロボットの巡回アクションに関する設定項目をまとめたクラスを実装。

---
+++ Assets/Script/Enemy/Scriptable/PatrolSettings.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+/// <summary>ロボットの巡回アクションに関する設定項目</summary>
+
+[CreateAssetMenu(fileName = "PatrolSettings", menuName = "ScriptableObjects/PatrolSettings")]
+
+public class PatrolSettings : ScriptableObject
+{
+    /// <summary>巡回する際の移動速度</summary>
+    [Header("巡回時の移動速度")] public float _patrolSpeed = 1f;
+
+    /// <summary>巡回する際の回転に要する時間</summary>
+    [Header("巡回時の回転時間")] public float _patrolRotationDuration = 1.5f;
+
+    /// <summary>巡回する目標地点を求める際の球の半径</summary>
+    [Header("巡回範囲の半径")] public float _patrolRadius = 5f;
+
+    /// <summary>巡回目標に到達したかを判定する閾値</summary>
+    [Header("巡回目標への到達閾値")] public float _patrolArrivalThreshold = 0.5f;
+
+    /// <summary>巡回目標を設定する際の最小回転角度</summary>
+    [Header("巡回目標への最小回転角度")] public float _minRotationAngle = 45f;
+
+    /// <summary>巡回する際にレイキャストを飛ばす距離</summary>
+    [Header("巡回時のレイキャストの距離")] public float _raycastDistance = 1.5f;
+}
