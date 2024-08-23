39b7326782f8ad2c948c932f78a867fc6d61cc58
KentoMatsuzaki <vantanstudent@outlook.jp>
2024-08-23 20:41:58 +0900

    [add] AttackSettings.cs
    ロボットクラスの攻撃情報を外部化したクラスを実装。

---
+++ Assets/Script/Enemy/Scriptable/AttackSettings.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+/// <summary>ロボットの攻撃アクションに関する設定項目</summary>
+[CreateAssetMenu(fileName = "AttackSettings", menuName = "ScriptableObjects/AttackSettings")]
+
+public class AttackSettings : ScriptableObject
+{
+    /// <summary>攻撃力</summary>
+    public int _power;
+
+    /// <summary>攻撃トリガー</summary>
+    public const string ATTACK_TRIGGER = "Attack";
+
+    /// <summary>攻撃エフェクトの生成位置のY座標オフセット</summary>
+    public float _attackEffectPositionOffsetY;   
+}
