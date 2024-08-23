ac8ca070c7cb41c6aec634ddba44a98302ae717a
KentoMatsuzaki <vantanstudent@outlook.jp>
2024-08-23 17:48:26 +0900

    [add] 敵の攻撃エフェクト
    敵の攻撃エフェクトのクラスを実装。
    直進と追尾の2パターンの処理を実装。

--- Assets/Script/Effect/Enemy/EnemyAttackEffect.cs
+++ Assets/Script/Effect/Enemy/EnemyAttackEffect.cs
@@ -3,5 +3,71 @@ using UnityEngine;
 /// <summary>敵の攻撃エフェクトにアタッチするクラス</summary>
 public class EnemyAttackEffect : MonoBehaviour
 {
-    
+    /// <summary>エフェクトの種類</summary>
+    public enum EnemyAttackEffectType
+    {
+        /// <summary>直進</summary>
+        Straight,
+
+        /// <summary>追尾</summary>
+        Homing
+    }
+
+    /// <summary>攻撃エフェクトの種類</summary>
+    [SerializeField, Header("攻撃エフェクトの種類")] private EnemyAttackEffectType _effectType;
+
+    /// <summary>攻撃エフェクトの移動速度</summary>
+    [SerializeField, Header("攻撃エフェクトの移動速度")] private float _moveSpeed;
+
+    /// <summary>追尾時の回転補完係数</summary>
+    [SerializeField, Header("追尾時の回転補完係数")] private float _rotationSLerpSpeed;
+
+    private void Update()
+    {
+        switch(_effectType)
+        {
+            // 直進するエフェクト
+            case EnemyAttackEffectType.Straight:
+                MoveForward(); break;
+
+            // 追尾するエフェクト
+            case EnemyAttackEffectType.Homing:
+                HomingPlayer(); break;
+        }
+    }
+
+    /// <summary>前方に移動する</summary>
+    private void MoveForward()
+    {
+        transform.Translate(transform.forward * _moveSpeed * Time.deltaTime);
+    }
+
+    /// <summary>プレイヤーを追尾する</summary>
+    private void HomingPlayer()
+    {
+        // プレイヤーの方向へ回転する
+        RotateTowardsPlayer();
+
+        // 前方に移動する
+        MoveForward();
+    }
+
+    /// <summary>プレイヤーへの方向を求める</summary>
+    private Vector3 GetDirectionToPlayer()
+    {
+        return (Player.Instance.transform.position - transform.position).normalized;
+    }
+
+    /// <summary>プレイヤーへの回転を求める</summary>
+    private Quaternion GetRotationToPlayer()
+    {
+        return Quaternion.LookRotation(GetDirectionToPlayer());
+    }
+
+    /// <summary>プレイヤーの方向へ回転する</summary>
+    private void RotateTowardsPlayer()
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, GetRotationToPlayer(),
+            _rotationSLerpSpeed * Time.deltaTime);
+    }
 }
